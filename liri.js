da20f31920f0fb3c8b589ce5f84fc551ecb0e678
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-04-14 12:46:53 -0400

    changing up method, adding requires

---
+++ liri.js
@@ -0,0 +1,11 @@
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs 		= require('fs');
+
+var spotify = new Spotify({
+	id: 'ad37e33f67e74a01b740d6a9c69fc4b3',
+	secret: '3296119a82294de6a18cd07aa249c59f'
+});
+
+
